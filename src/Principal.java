a8975a0dce534fd0c2c4f31d4bf444063aa61e0b
Ignaciogg <nachogilgarzon@gmail.com>
2021-04-22 13:20:30 +0200

    cambios Principal

--- src/Principal.java
+++ src/Principal.java
@@ -44,42 +44,47 @@ public class Principal {
 
         Scanner input = new Scanner(System.in);
         Boolean inicioSesion = false;
+        Boolean usuarioEncontrado = false;
         int tipo_usuario=0;
         String email, contraseña;
 
-        System.out.println("   --- Bienvenido a MediTech (varsión 1.0) --- \n");
+        System.out.println("   --- Bienvenido a MediTech (varsión 1.0) --- \n" +
+                "\n" +
+                "            \t 1) Recepcionista \n" +
+                "            \t 2) Paciente      \n" +
+                "            \t 3) Medico        \n" +
+                "            \t 4) Administrador \n");
 
         do {
             //Preguntamos el tipo de usuario
             System.out.print(
-                    "\t 1) Recepcionista\n" +
-                    "\t 2) Paciente\n" +
-                    "\t 3) Medico\n" +
-                    "\t 4) Administrador\n" +
                     "Introduce el tipo de usuario con el que vas a iniciar sesion: ");
 
-            if(input.hasNextInt())
+            if(input.hasNextInt()){
                 tipo_usuario = input.nextInt();
+                usuarioEncontrado = true;
 
+
+                input.nextLine(); //limpiar buffer Scanner
+
+                //Solicitamos los datos de inicio de sesion
+                System.out.print("Introduce el email:");
+                email = input.nextLine();
+                System.out.print("Introduce la contraseña:");
+                contraseña = input.nextLine();
+
+                inicioSesion = iniciarSesion(tipo_usuario, email, contraseña);
+                if (inicioSesion) {
+                    System.out.print("Has iniciado sesión correctamente");
+                } else {
+                    System.out.print("Los datos introducidos no son correctos");
+                }
+            }
             else
                 System.out.println("ERROR al introducir el tipo de usuario");
 
 
-            input.nextLine(); //limpiar buffer Scanner
-
-            //Solicitamos los datos de inicio de sesion
-            System.out.print("Introduce el email:");
-            email = input.nextLine();
-            System.out.print("Introduce la contraseña:");
-            contraseña = input.nextLine();
-
-            inicioSesion = iniciarSesion(tipo_usuario, email, contraseña);
-            if (inicioSesion) {
-                System.out.print("Has iniciado sesión correctamente");
-            } else {
-                System.out.print("Los datos introducidos no son correctos");
-            }
-        }while (!inicioSesion);
+        }while (!inicioSesion && !usuarioEncontrado);
 
     }
 }
